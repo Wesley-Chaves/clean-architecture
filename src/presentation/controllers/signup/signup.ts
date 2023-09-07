84453f0929416a55bcdfff2c87c4becdcf19a184
Wesley Spectre <wesleychavesdev@gmail.com>
2023-09-06 22:14:17 -0300

    refactor: add types in SignUpController and in the handle method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,9 @@
 import { MissingParamError } from '../../errors/missing-param'
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
 
-export class SignUpController {
-  handle (httpRequest: any): any {
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
